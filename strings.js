f6e50c3c28b89721aa9a8910ad99031f2bffc3fa
lekishonsheila <lekishonsheila@gmail.com>
2023-03-12 17:44:32 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,48 @@
+//first four characters
+let a = "beautiful";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+/*I've invited you to fill out a form:
+Strings
+Extract the first four characters from the string below;
+"beautiful"
+Insert the following string at the tenth index of the below variable:
+"eat"*/
+const food = "I did not have appetite today"
+let action = "eat"
+console.log(food[10]+``,action);
+/*Count how many times the following string appears in the string variable:
+1. "the"
+2."s"
+const story= "She sells sea shells at the sea shore"*/
+const story= "She sells sea shells at the sea shore"
+/*Convert the following strings into the specified format:
+1. UpperCase: "CONfidant"
+2. LowerCase: "amazing", "beautiFUL"
+3. Title case "A busy office" */
+let word = "CONfidant";
+let word2 = ("amazing", "beautiFUL");
+let sentence = "A busy office";
+console.log(word.toUpperCase);
+console.log(word2.toLowerCase);
+/*Using JavaScript, find the following words from the following strings:
+1. "market"
+const string1 = "The lady went to the market with her sister";
+2. "season"
+const string2 = "My favorite season is spring"
+ */
+const string1 = "The lady went to the market with her sister";
+console.log(string1.indexOf("market"));
+const string2 = "My favorite season is spring"
+console.log(string2.indexOf("season"));
+
+
+
+
+
+
+
+
+
